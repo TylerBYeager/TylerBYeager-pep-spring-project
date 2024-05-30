ae71dad1211df4f04fc676a66ec48fe1e3ecf245
Tyler Brian Yeager <wow_d2@hotmail.com>
2024-05-30 19:48:36 +0000

    Added code to repositories, extended JpaRepositories

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,8 @@
 package com.example.repository;
+import com.example.entity.Account;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository  extends JpaRepository<Account, Integer>{
+
 }
