37bb587757ee153f56ce7d2b928851e4918c5d3d
Tyler Brian Yeager <wow_d2@hotmail.com>
2024-06-01 18:16:59 +0000

    removed unused imports

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,10 +1,8 @@
 package com.example.repository;
 import com.example.entity.Account;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository  extends JpaRepository<Account, Integer>{
 
